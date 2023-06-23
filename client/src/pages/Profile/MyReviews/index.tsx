db0ed49719f0fac5bbc5da80c5a57067e8678758
berezenko04 <114209763+Sandrela1@users.noreply.github.com>
2023-06-23 18:25:38 +0300

    update: updated reviews page in profile

--- client/src/pages/Profile/MyReviews/index.tsx
+++ client/src/pages/Profile/MyReviews/index.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
 import { isMobile } from 'react-device-detect'
-import { useSelector } from 'react-redux'
-import { useAppDispatch } from '@/redux/store'
 
 //styles
 import styles from './MyReviews.module.scss'
@@ -13,33 +11,41 @@ import CommentSkeleton from '@/components/Skeletons/CommentSkeleton'
 import Pagination from '@/components/Pagination'
 
 //redux
-import { myReviewsSelector } from '@/redux/myReviews/selectors'
-import { fetchMyReviews } from '@/redux/myReviews/asyncActions'
+import { TComment } from '@/redux/comments/types'
 
+//service
+import { getUserReviews } from '@/API/userService'
 
 const MyReviews: React.FC = () => {
-    const dispatch = useAppDispatch();
+    const [isLoading, setIsLoading] = useState(true);
+    const [reviews, setReviews] = useState<TComment[]>();
+    const [count, setCount] = useState<number>(0);
     const [page, setPage] = useState(1);
     const limit = isMobile ? 6 : 8;
-    const { items, status, length } = useSelector(myReviewsSelector);
-    const pageCount = Math.ceil(length / limit);
+    const pageCount = Math.ceil(count / limit);
 
     const handlePageChange = (newPage: number) => {
         setPage(newPage);
     }
 
     useEffect(() => {
-        dispatch(fetchMyReviews({ page, limit }));
-    }, [page, limit])
+        setIsLoading(true);
+        (async () => {
+            const data = await getUserReviews(page, limit);
+            setReviews(data.comments);
+            setCount(data.length);
+        })();
+        setIsLoading(false);
+    }, [reviews]);
 
     return (
         <div className={styles.reviews}>
             <ProfileLayout>
                 <div className={styles.reviews__wrapper}>
-                    <h3>My reviews ({length ? length : 0})</h3>
+                    <h3>My reviews ({count})</h3>
                     <div className={styles.reviews__main}>
-                        {status !== 'loading' ?
-                            items.map((review, index) => (
+                        {!isLoading ?
+                            reviews && reviews.map((review, index) => (
                                 <div className={styles.reviews__main__item} key={index} >
                                     <Comment {...review} />
                                 </div>
